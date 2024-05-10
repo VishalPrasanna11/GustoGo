c216f65cc6b10f196e71c54bc290a96c1036b359
vishalprasanna11 <prasanna.vi@northeastern.edu>
2024-05-10 19:01:36 -0400

    Added Mobile Nav bar

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
     return (
-        <header className="border-b-2 border-b-#0A0A0A py-6">
-            <div className="container mx-auto flex justify-between items-center py-4">
+        <header className="border-b-2 border-b-#0A0A0A py-4">
+            <div className="container mx-auto flex justify-between items-center">
                 <Link to="/" className="text-#0A0A0A  text-3xl tracking-tight font-bold">GustoGo</Link>
+                <div className="md:hidden">
+                    <MobileNav/>
+                </div>
             </div>
         </header>
     );
