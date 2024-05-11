4084a60b78abb7aa7b19cbee26f97cb1209d68b3
SAI SRUNITH SILVERY <silvery.s@northeastern.edu>
2024-05-11 19:27:02 -0400

    user api

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,13 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/my/user", userRoute);
 
 mongoose.connect(process.env.MONGO_URI as string)
 .then(() => console.log("Connected to MongoDB"))
@@ -13,9 +16,9 @@ mongoose.connect(process.env.MONGO_URI as string)
 
 
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
-});
+// app.get("/", (req: Request, res: Response) => {
+//   res.send("Hello World!");
+// });
 
 const PORT = process.env.PORT || 7001;
 
---
+++ server/src/model/userModel.ts
@@ -0,0 +1,28 @@
+import mongoose from"mongoose";
+
+const userSchema = new mongoose.Schema({
+    auth0id: {
+        
+            type: String,
+            required: true,
+        },
+
+        email:{
+            type: String,
+        },
+        addressLine1:
+        {
+            type: String,
+        },
+        city:{
+            type: String,
+        },
+        country:{
+            type: String,
+        },
+    }
+
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
