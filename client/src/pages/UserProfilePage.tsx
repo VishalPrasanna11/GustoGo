803600938bcf8df612ebace7382400418f676843
vishalprasanna11 <prasanna.vi@northeastern.edu>
2024-07-16 20:14:39 -0400

    New Changes on User Profile

--- client/src/pages/UserProfilePage.tsx
+++ client/src/pages/UserProfilePage.tsx
@@ -12,27 +12,7 @@ const UserProfilePage = () => {
   if (!currentUser) {
     return <span>Unable to load user profile</span>;
   }
-  const handleSave = async (formData: UserFormData) => {
-    try {
-      // Assuming updateUser expects a complete user profile data object
-      await updateUser({
-        name: formData.name,
-        addressLine1: formData.addressLine1,
-        addressLine2: formData.addressLine2,
-        city: formData.city,
-        country: formData.country,
-        state: formData.state,
-        phone: formData.phone,
-        zipCode: formData.zipCode,
-      
-        // Add other required fields like addressLine2, state, zipCode, phone if necessary
-      });
-      // Optionally handle success, e.g., show a success message
-    } catch (error) {
-      console.error('Error updating user profile:', error);
-      // Handle error, e.g., show an error message
-    }
-  };
+ 
   return (
     <UserProfileForm
       currentUser={currentUser}
